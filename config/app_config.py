eec3c6aaafc072f5e4ec0b807d8a8f1f86446e40
Janagani Sai Varshith <saivarshithjanagani@gmail.com>
2026-07-25 22:48:33 +0530

    Fix config

--- config/app_config.py
+++ config/app_config.py
@@ -11,6 +11,7 @@ class AppConfig:
     nessie_ref: str = "main"
     nessie_auth_type: str = "NONE"
     iceberg_warehouse: str = "s3://finlake-warehouse/warehouse"
+    s3_endpoint: str = "http://finlake-minio:9000"
     aws_region: str = "us-east-1"
     aws_access_key_id: str = "minioadmin"
     aws_secret_access_key: str = "minioadmin"
@@ -22,6 +23,7 @@ class AppConfig:
             nessie_ref=os.getenv("NESSIE_REF", "main"),
             nessie_auth_type=os.getenv("NESSIE_AUTH_TYPE", "NONE"),
             iceberg_warehouse=os.getenv("ICEBERG_WAREHOUSE", "s3://finlake-warehouse/warehouse"),
+            s3_endpoint=os.getenv("S3_ENDPOINT", "http://finlake-minio:9000"),
             aws_region=os.getenv("AWS_REGION", "us-east-1"),
             aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", "minioadmin"),
             aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", "minioadmin"),
