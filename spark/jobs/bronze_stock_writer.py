cacd89751c22b5878d33be6bd248caf3560522ae
Janagani Sai Varshith <saivarshithjanagani@gmail.com>
2026-07-28 06:44:02 +0530

    Add sample US Stocks tickers

--- spark/jobs/bronze_stock_writer.py
+++ spark/jobs/bronze_stock_writer.py
@@ -1,5 +1,6 @@
 from spark.handler import iceberg_initialisation, build_spark
-
+import yfinance as yf
+from spark.model.constants import popular_tickers
 
 
 @iceberg_initialisation
@@ -8,6 +9,10 @@ def main():
     spark.sparkContext.setLogLevel("WARN")
 
     print("=" * 80)
+    tickers = popular_tickers[:20]
+    df = yf.download(tickers, period="5d", group_by="ticker")
+    df.columns = df.columns.swaplevel(0, 1)
+
     print("FinLake Bronze Stock Writer Job Started Successfully")
     print(f"Spark version       : {spark.version}")
     print("=" * 80)
---
+++ spark/model/constants.py
@@ -0,0 +1,55 @@
+popular_tickers = [
+    "AAPL",   # Apple
+    "MSFT",   # Microsoft
+    "NVDA",   # NVIDIA
+    "AMZN",   # Amazon
+    "GOOG",   # Alphabet Class C
+    "GOOGL",  # Alphabet Class A
+    "META",   # Meta Platforms
+    "TSLA",   # Tesla
+    "AVGO",   # Broadcom
+    "NFLX",   # Netflix
+    "AMD",    # AMD
+    "INTC",   # Intel
+    "PLTR",   # Palantir
+    "ORCL",   # Oracle
+    "CRM",    # Salesforce
+    "ADBE",   # Adobe
+    "CSCO",   # Cisco
+    "QCOM",   # Qualcomm
+    "IBM",    # IBM
+    "MU",     # Micron
+
+    "JPM",    # JPMorgan Chase
+    "BAC",    # Bank of America
+    "WFC",    # Wells Fargo
+    "GS",     # Goldman Sachs
+    "MS",     # Morgan Stanley
+    "V",      # Visa
+    "MA",     # Mastercard
+    "AXP",    # American Express
+    "BRK-B",  # Berkshire Hathaway Class B
+    "BLK",    # BlackRock
+
+    "LLY",    # Eli Lilly
+    "JNJ",    # Johnson & Johnson
+    "PFE",    # Pfizer
+    "MRK",    # Merck
+    "ABBV",   # AbbVie
+    "UNH",    # UnitedHealth
+    "ISRG",   # Intuitive Surgical
+    "TMO",    # Thermo Fisher
+    "VRTX",   # Vertex Pharmaceuticals
+    "AMGN",   # Amgen
+
+    "COST",   # Costco
+    "WMT",    # Walmart
+    "HD",     # Home Depot
+    "MCD",    # McDonald's
+    "DIS",    # Disney
+    "KO",     # Coca-Cola
+    "PEP",    # PepsiCo
+    "NKE",    # Nike
+    "UBER",   # Uber
+    "SHOP"    # Shopify
+]
